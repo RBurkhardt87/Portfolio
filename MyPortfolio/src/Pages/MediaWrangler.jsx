89451c1c14ec73ad4d4df41827e990813b633007
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2025-04-15 19:43:15 -0500

    Almos done with Media Wrangler Page. Need to add a few more pics and get a demo link for it

--- MyPortfolio/src/Pages/MediaWrangler.jsx
+++ MyPortfolio/src/Pages/MediaWrangler.jsx
@@ -8,112 +8,119 @@ const MediaWrangler = () => {
     <div>
       <div className='project-info-container'></div>
         <h1>Media Wrangler Application</h1>    
-
         <br />
         <br />
 
-        <h4>What is Media Wrangler?</h4>
-        <p>
-        Media Wrangler is a full-stack movie-tracking app that helps users discover, organize, and engage with films—all in one place.</p>
-        <p><b>Users can log in to:</b></p>
-        <ul>
-          <li>Track and rate movies</li>
-          <li>Create custom watchlists</li>
-          <li>Write and comment on reviews</li>
-          <li>Discover where movies are streaming</li>
-          <li>Stay updated on upcoming releases</li>
-          <li>Connect with others in the community</li>
-        </ul>
-
-        <br />
-        <br />
-
-        <h4>Project Links</h4>
-        <ul>
-          <li><b>GitHub</b> — <a href="https://github.com/RBurkhardt87/media-wrangler" target="_blank" >Original Repo.</a> </li>
-          <li><b>GitHub</b> — <a href="https://github.com/RBurkhardt87/media-wrangler-expand" target='_blank'>Expansion Repo</a> </li>
-          <li><b>Project Demo Video</b> — </li>
-          <li><b> Full Project Presentation</b> — <a href="https://docs.google.com/presentation/d/1Q6HOewM0bPnU61V9ypXlkjknPRM4q6VMtil-CI6yPhs/present" target="_blank" >Media Wrangler Slideshow</a></li>
-        </ul>
-
-        <br />
-        <br />
-
-        <h4>Why We Built It:</h4>
-        <p>The idea behind Media Wrangler stemmed from a common frustration: finding comprehensive and reliable movie information often requires visiting multiple sites. Whether you're trying to locate where a movie is streaming, learn about upcoming releases, or keep track of films you want to watch, the process can feel fragmented and time-consuming. Media Wrangler solves this by offering a one-stop shop for movie enthusiasts. 
-        </p>
-        <p><b>Our goals were to: </b></p>
-        <ul>
-          <li>Simplify movie discovery</li>
-          <li>Help users manage their watch habits</li>
-          <li>Encourage connection through shared opinions and recommendations</li>
-        </ul>
-
-        <br />
-        <br />
-
-        <h4>Tech Stack:</h4>        
-        <p><b>Backend: </b>Java, Spring Boot, Hibernate, MySQL</p>        
-        <p><b>Frontend: </b>React, Material-UI (MUI), Formik</p>        
-        <p><b>External API: </b>The Movie Database (TMDB)</p>
-
-        <br />
-        <br />
-
-        <h4>Core Features:</h4>
-        <ul>
-          <li>User authentication and account management</li>
-          <li>Movie search with streaming info</li>
-          <li>Create custom lists (e.g., Favorites, Watchlist)</li>
-          <li>Write, like, and comment on reviews</li>
-          <li>Journal of all user reviews (with the ability to edit and delete reviews)</li>
-          <li>Random movie generator </li>
-          <li>Track upcoming releases with calendar integration</li>
-          <li>Community discussion board</li>
-        </ul>
-
-        <br />
-        <br />
-
-        <h4>UI Highlights: </h4>
-        <p>Include a few select screenshots here with a short caption per image </p>
-        <ul>
-          <li>Movie Details Page: this will include the api data being returned, the user interaction card</li>
-          <li>Review Form: Shows the movie review form and all its fields </li>
-          <li>Movie Reviews: show users commenting and responding on each others movie reviews </li>
-        </ul>
-
-        <br />
-        <br />
-
-
-
-        <div className='db-section'>
-          <h4>Database Overview: </h4>  
-          <p>Media Wrangler uses a relational MySQL database with 10 tables centered around the User table. All content—reviews, ratings, likes, discussions, events, and movie lists—is tracked by user_id, ensuring personalized and secure user data.</p>
-          <div className='pic-container'>
-            <ImagePopup img="/public/images/DBpicture.png" caption="Click Image to Enlarge" />
-            <ImagePopup img="/public/images/DBpicture2.png" caption="Click Image to Enlarge"  />
+        <div className='section-container'> 
+          <div className='section-text'>
+            <h4>What is Media Wrangler?</h4>            
+            <p>Media Wrangler is a full-stack movie-tracking app that helps users discover, organize, and engage with films—all in one place.</p>
+            <p><b>Users can log in to:</b></p>
+            <ul>
+              <li>Track and rate movies</li>
+              <li>Create custom watchlists</li>
+              <li>Write and comment on reviews</li>
+              <li>Discover where movies are streaming</li>
+              <li>Stay updated on upcoming releases</li>
+              <li>Connect with others in the community</li>
+            </ul>
           </div>
-          <p><b>Key relationships:</b></p>
-          <ul>
-            <li>Users can: like, rate, and review movies</li>
-            <li>Reviews can: have multiple comments and replies</li>
-            <li>Users can: participate in discussion boards by asking and answering questions</li>
-            <li>Custom movie lists and event tracking are tied to the user account</li>
-          </ul>           
-          <p>This schema allows for dynamic content, community engagement, and data integrity, supporting features like movie detail pages with like counts, personalized lists, user reviews with ratings, and comment threads.</p>          
+          <div className='section-text'>
+            <h4>Why We Built It:</h4>
+            <p>The idea behind Media Wrangler stemmed from a common frustration: finding comprehensive and reliable movie information often requires visiting multiple sites. Whether you're trying to locate where a movie is streaming, learn about upcoming releases, or keep track of films you want to watch, the process can feel fragmented and time-consuming. Media Wrangler solves this by offering a one-stop shop for movie enthusiasts. 
+            </p>
+            <p><b>Our goals were to: </b></p>
+            <ul>
+              <li>Simplify movie discovery</li>
+              <li>Help users manage their watch habits</li>
+              <li>Encourage connection through shared opinions and recommendations</li>
+            </ul>
+          </div>
         </div>
+        <br />
+        <br />
         
+        <div className='section-stack'>
+          <div className='section-text'>
+            <h4>Project Links</h4>          
+            <ul>
+              <li><b>GitHub</b> — <a href="https://github.com/RBurkhardt87/media-wrangler" target="_blank" >Original Repo.</a> </li>
+              <li><b>GitHub</b> — <a href="https://github.com/RBurkhardt87/media-wrangler-expand" target='_blank'>Expansion Repo</a> </li>
+              <li><b>Project Demo Video</b> — </li>
+              <li><b>Project Presentation</b> — <a href="https://docs.google.com/presentation/d/1Q6HOewM0bPnU61V9ypXlkjknPRM4q6VMtil-CI6yPhs/present" target="_blank" >Media Wrangler Slideshow</a></li>
+            </ul>
+          </div>
+          <br />
+          <div className='section-text'>
+            <h4>Tech Stack:</h4>   
+            <ul>
+              <li><b>Backend: </b>Java, Spring Boot, Hibernate, MySQL</li>
+              <li><b>Frontend: </b>React, Material-UI (MUI), Formik</li>
+              <li><b>External API: </b>The Movie Database (TMDB)</li>
+            </ul>     
+          </div>          
+        </div>
+        <br />
+        <br />      
+
+        <div className='section-container'>    
+          <div className='section-text'>
+            <h4>Core Features:</h4>
+            <ul>
+              <li>User authentication and account management</li>
+              <li>Movie search with streaming info</li>
+              <li>Create custom lists (e.g., Favorites, Watchlist)</li>
+              <li>Write, like, and comment on reviews</li>
+              <li>Journal of all user reviews (with the ability to edit and delete reviews)</li>
+              <li>Random movie generator </li>
+              <li>Track upcoming releases with calendar integration</li>
+              <li>Community discussion board</li>
+            </ul>
+          </div>
+        </div>
         <br />
         <br />
 
-        <div className='planning-sec'>
-          <h4>Planning & Process: </h4>
+        <div className='section-container'>
+          <div className='section-text'>
+            <h4>UI Highlights: </h4>
+            <p>Include a few select screenshots here with a short caption per image </p>
+            <ul>
+              <li>Movie Details Page: this will include the api data being returned, the user interaction card</li>
+              <li>Review Form: Shows the movie review form and all its fields </li>
+              <li>Movie Reviews: show users commenting and responding on each others movie reviews </li>
+            </ul>
+          </div>
+        </div>     
+        <br />
+        <br />
 
-          <div className='section-container'>
-            <ImagePopup img="/public/images/UserStory2.png" caption="Click Image to Enlarge" className="float-left"/>
-            <div className='planning-text'>
+        <div className='section-container'>
+          <div className='section-text'>
+            <h4>Database Overview: </h4>  
+            <p>Media Wrangler uses a relational MySQL database with 10 tables centered around the User table. All content—reviews, ratings, likes, discussions, events, and movie lists—is tracked by user_id, ensuring personalized and secure user data.</p>
+            <div className='pic-container'>
+              <ImagePopup img="/public/images/DBpicture.png" caption="Click Image to Enlarge" />
+              <ImagePopup img="/public/images/DBpicture2.png" caption="Click Image to Enlarge"  />
+            </div>
+            <p><b>Key relationships:</b></p>
+            <ul>
+              <li>Users can: like, rate, and review movies</li>
+              <li>Reviews can: have multiple comments and replies</li>
+              <li>Users can: participate in discussion boards by asking and answering questions</li>
+              <li>Custom movie lists and event tracking are tied to the user account</li>
+            </ul>           
+            <p>This schema allows for dynamic content, community engagement, and data integrity, supporting features like movie detail pages with like counts, personalized lists, user reviews with ratings, and comment threads.</p>          
+          </div>
+        </div>      
+        <br />
+        <br />
+
+        
+          <h4>Planning & Process:</h4>
+          <div className='section-container right'>
+            <ImagePopup img="/public/images/UserStory2.png" caption="Click Image to Enlarge" />
+            <div className='section-text'>
               <p><b>User Story: </b>as a User, I want to be able to click on "add review" button, so that I can write my opinion of the movie</p>
               <p><b>AC:</b></p>
               <ol>
@@ -134,15 +141,13 @@ const MediaWrangler = () => {
                 </ul>
               </ol>
             </div>
-          </div>  
-
+          </div>         
           <br />
           <br />      
-          <br /> 
 
-          <div className='section-container '>
-            <ImagePopup img="/public/images/UserStory1.png" caption="Click Image to Enlarge" className="float-left"/>
-            <div className='planning-text'>
+          <div className='section-container'>
+            <ImagePopup img="/public/images/UserStory1.png" caption="Click Image to Enlarge"/>
+            <div className='section-text'>
               <p><b>User Story: </b>as a User, I want to be able to see the details of the movie I searched, so I can learn more about the movie and/or have journal interactions with the movie.</p>
               <p><b>AC:</b></p>
               <ol>
@@ -177,34 +182,35 @@ const MediaWrangler = () => {
                 </ul>
               </ol>
             </div>
-          </div>           
-        </div>
-
-        <br />
-        <br />
+          </div>                    
+          <br />
+          <br />
   
-        <h4>Future Improvements</h4>
-        <p>Media Wrangler, although functional, is still a work in progress. We discussed furthering the application's features such as: </p>
-        <ul>
-          <li>Expand the calendar feature to include Google Calendar</li>
-          <li>Add more media options like tv shows with episode progress feature</li>
-          <li>Add social feature including friends and follow</li>
-          <li>Implement local showtimes and ticket booking</li>
-          <li>Monetize links to streaming platforms</li>
-          <li>Replace sessions with JWT for better security and scalability</li>
-        </ul>
-        <p>Since launch, I've expanded movie review threads so users can now directly reply to specific comments rather than seeing one long discussion thread.</p>
+          <div className='section-container'>
+            <div className='section-text'>
+              <h4>Future Improvements</h4>            
+              <p>Media Wrangler, although functional, is still a work in progress. We discussed furthering the application's features such as: </p>
+              <ul>
+                <li>Expand the calendar feature to include Google Calendar</li>
+                <li>Add more media options like tv shows with episode progress feature</li>
+                <li>Add social feature including friends and follow</li>
+                <li>Implement local showtimes and ticket booking</li>
+                <li>Monetize links to streaming platforms</li>
+                <li>Replace sessions with JWT for better security and scalability</li>
+              </ul>
+              <p>Since LaunchCode, I've expanded movie review threads so users can now directly reply to specific comments rather than seeing one long discussion thread.</p>
+            </div>
+            <br />
+            <br />
+            <div className='section-text'>
+              <h4>Final Thoughts: </h4>
+              <p>Media Wrangler reflects how far I've come in full-stack development—from backend architecture to thoughtful UI design to user    engagement. It also demonstrates how collaboration and planning (especially in group projects) elevate functionality and experience.</p>
+            </div>
+          </div>
+          <br />
+          <br />
 
-        <br />
-        <br />
-        
-        <h4>Final Thoughts: </h4>
-        <p>Media Wrangler reflects how far I've come in full-stack development—from backend architecture to thoughtful UI design to user engagement. It also demonstrates how collaboration and planning (especially in group projects) elevate functionality and experience.</p>
-
-        <br />
-        <br />
-
-        <p><b>Back to </b><a href="/projects">Project List</a></p>     
+          <h4><b>Back to </b><a href="/projects">Project List</a></h4>     
     </div>
   )
 }
